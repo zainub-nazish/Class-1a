4e75e81ee73230142a999956c77a7a5513e811a3
zainub-nazish <160176214+zainub-nazish@users.noreply.github.com>
2025-02-28 09:58:55 +0500

    Add files via upload

---
+++ main.py
@@ -0,0 +1,70 @@
+name: str = '(Governor i for ai computing)'
+print("GIAIC stand for : ", name)
+print("Hello World from Asharib")
+
+def giaic():
+ print("Hello world from student")
+
+# ///////////////////////////////////////////
+
+str = "Hello world"
+print(str)
+
+# /////////////////////////////////////////
+
+if type(str):
+    print("This is a string")
+
+    str = "Hello world"
+    print(str)
+
+    if True:
+        print('This is a string')
+
+# /////////////////////////////////////
+
+text="hello world"
+print(text)
+
+id_card="""Name: Zainub Nazish
+Father Name: Muhammad Tajammul Haque
+Age:20
+"""
+print(id_card)
+
+
+
+# /////////////////////////////////////////////
+
+num:int=35
+print(type(num))
+
+float_num:float=3.5
+print(type(float_num))
+
+# ////////////////////////////////
+
+
+is_true:bool=True
+is_falseP:bool=False
+print(type(is_true))
+
+# /////////////////////////////////
+
+
+
+fruit:list=["apple","cherry"]
+print(type(fruit))
+print(fruit[0])
+print(fruit[-1])
+
+
+# ////////////////////////////////////
+
+tuple_data:tuple=("apple","cherry")
+print(type(tuple_data))
+print(tuple_data[0])
+print(tuple_data[-1])
+
+# //////////////////////////////////
+
